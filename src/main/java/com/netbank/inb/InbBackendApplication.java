290a75e13c446303a1f1c9ab3638e23db7fa1827
unknown <shubham.bane13@gmail.com>
2026-02-01 11:47:20 +0530

    initial commit

---
+++ src/main/java/com/netbank/inb/InbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.netbank.inb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netbank/inb/InbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netbank.inb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
